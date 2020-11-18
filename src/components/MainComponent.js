fa9b93cc8b6b0eba3413621dd9b2b71465f18973
Evans <evans@Evans-Laptop.Evans>
2020-11-18 20:00:21 +0200

    ControlledForms 2

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,63 +1,71 @@
 import React, { Component } from 'react';
-import Header from './HeaderComponent'
+import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './MenuComponents';
 import DishDetail from './DishdetailComponent';
 import { DISHES } from '../shared/dishes';
 import { COMMENTS } from '../shared/comments';
-import { LEADERS } from '../shared/leaders';
 import { PROMOTIONS } from '../shared/promotions';
-import Footer from './Footer.Component';
+import { LEADERS } from '../shared/leaders';
+import Header from './HeaderComponent';
 import Home from './HomeComponent';
-import {Switch,Route,Redirect}  from 'react-router-dom';
+import Footer from './Footer.Component';
 import Contact from './ContactComponent';
+import About from './AboutComponent'
+
+import { Switch, Route, Redirect } from 'react-router-dom';
+
 
 class Main extends Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-        dishes: DISHES,
-       comments:COMMENTS,
-       leaders:LEADERS,
-       promotions:PROMOTIONS
-    };
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            dishes: DISHES,
+            comments: COMMENTS,
+            promotions: PROMOTIONS,
+            leaders: LEADERS
+        };
+    }
 
+    render() {
 
-  render() {
+        const HomePage = () => {
+            return (
+                <Home dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+                promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+                leader={this.state.leaders.filter((leader) => leader.featured)[0]}/>
+            );
+        }
 
-    const HomePage = () => {
-        return(
-            <Home dish={this.state.dishes.filter((dish) => dish.featured)[0]}
-            promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
-           leader={this.state.leaders.filter((leader) => leader.featured)[0]}
-            />
-        );
-      }
+        const DishWithId = ({match}) => {
+            return(
+                <DishDetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]} 
+                  comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))} />
+            );
+          };
 
-      const DishWithId = ({match}) =>{
+        const Leaders = () =>{
+            return(
+                <About leaders={this.state.leaders} />
+            )
+        }
+
         return (
-          <DishDetail dish={this.state.dishes.filter((dish) => dish.id===parseInt(match.params.dishId,10))[0]}
-             comments={this.state.comments.filter((comment) => comment.dishId===parseInt(match.params.dishId,10))}
-             
-             />
-        );
-      }
-    return (
-      <div>
-       <Header />
-       <Switch>
-              <Route path="/home " component={HomePage} />
-              <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
-              <Route path="/menu/:dishId" component={DishWithId} />
-              <Route exact path="/contactus" component={Contact} />
-              <Redirect to="/home" />
-          </Switch>
-        
-        <Footer />
-     </div>
-    );
-  }
+            <div>
+                <Header />
+                <Switch>
+                    <Route path='/home' component={HomePage} />
+                    <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
+                    <Route path="/menu/:dishId" component={DishWithId} />           
+                    <Route path="/aboutus" component={() => <About leaders={this.state.leaders} />} />          
+                    <Route exact path='/contactus' component={Contact} />
+                    <Redirect to="/home" />
+                </Switch>
+                <Footer />
+            </div>
+        )
+
+    }
 }
 
 export default Main;
